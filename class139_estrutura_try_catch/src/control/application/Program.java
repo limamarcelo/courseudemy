b91811ae02aaf081a33dcc3bce223d84b04d2630
Marcelo Miranda de Lima <miranda.rs@gmail.com>
2021-01-22 05:48:03 -0300

    small adjustment class139_estrutura_try_catch

--- class139_estrutura_try_catch/src/control/application/Program.java
+++ class139_estrutura_try_catch/src/control/application/Program.java
@@ -1,4 +1,4 @@
-package class139_estrutura_try_catch;
+package control.application;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -12,8 +12,7 @@ public class Program {
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
-			System.out.println(vect[position]);
-			
+			System.out.println(vect[position]);	
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.err.println("Invalid position");
